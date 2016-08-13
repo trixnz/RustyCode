44fb831da3f415c764a698fe31c1b8f450dfc555
trixnz <me@trix.nz>
2016-08-13 23:23:37 +0200

    Added a prompt for the user to install the required tools we depend on for extended functionality. Fixes #141 Heavily influenced by the Go extension which seems to do it nicely.

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,7 @@ import PathService from './services/pathService';
 import {CommandService, ErrorFormat} from './services/commandService';
 import WorkspaceSymbolService from './services/workspaceSymbolService';
 import DocumentSymbolService from './services/documentSymbolService';
+import offerToInstallTools from './installTools';
 
 export function activate(ctx: vscode.ExtensionContext): void {
     // Set path to Rust language sources
@@ -117,6 +118,7 @@ export function activate(ctx: vscode.ExtensionContext): void {
     }));
 
     updateErrorFormatFlags();
+    offerToInstallTools();
 
     // Commands
     // Cargo build
